938e436d6a5b9189e897093dacc56ca9bea06425
unknown <vijaymurte2021@gmail.com>
2026-05-07 16:50:26 +0530

    asf

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py seed_cities
+
 python manage.py migrate
+
+python manage.py collectstatic --noinput
+
+python manage.py seed_cities
--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1e49be-g==oxzx*fn5x&bq!z%)jm&el3rax47bc+1pbv%4zuyc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['python-project-1-u510.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
